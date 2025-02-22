115df096b6300cec1dc22685148d7ddeae100d8e
anasgee <anasraza6096@gmail.com>
2025-02-22 09:30:44 +0500

    added remove button functionality

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -14,6 +14,47 @@ const createProduct = async(req,res,next)=>{
  }
 }
 
+const updateProduct = async(req,res,next)=>{
+   const productRef = await Product.findById(req.params.id);
+   if(!productRef){
+      return res.status(404).json({
+         success:false,
+         message:"Product Not Found"
+      })
+   }
+   // // // // // // // //
+   const updateProdct = await Product.findByIdAndUpdate(req.params.id, req.body,{
+      new:true,
+      runValidators:true,
+      useFindAndModify:false
+   });
+
+   res.status(201).json({
+      success:true,
+      message:"Document updated successfully",
+      updateProdct
+   })}
+
+// Delete a product
+
+
+const deleteProduct = async(req,res,next)=>{
+
+   try {
+      const product = await Product.findById(req.params.id);
+if(!product){
+  return res.status(404).json({success:false,message:"Product Not Found"});
+}
+
+await product.deleteOne();
+res.status(200).json({success:true,message:"Product Removed Successfully"})
+
+   } catch (error) {
+      res.status(500).json({success:false,message:`Error here and error is ${error}`})
+   }
+
+}
+
 
 
 // Get All products
@@ -27,4 +68,9 @@ const getAllProducts = async(req,res,next)=>
 
 
 
-module.exports ={getAllProducts,createProduct}
+
+
+
+
+
+module.exports ={getAllProducts,createProduct,updateProduct,deleteProduct}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productControllers");
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/productControllers");
 const router  = express.Router();
 
 
 router.get("/product",getAllProducts);
 router.post("/product/new",createProduct);
+router.put("/product/:id",updateProduct)
+router.delete("/product/:id",deleteProduct)
 
 
 module.exports=router;
